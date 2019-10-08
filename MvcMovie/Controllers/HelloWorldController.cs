42b451d28f788beec50b7ba144a4a2ddc58caa0d
marco121942 <marco.sotomayor@tecsup.edu.pe>
2019-10-07 22:19:27 -0500

    first commit

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        /*
+        // GET: HelloWorld
+        public ActionResult Index()
+        {
+            return View();
+        }
+        */
+        /*
+        public string Index()
+        {
+            return "Esta es mi accion por <b< Defecto</b>....";
+        }
+        */
+        //public string Welcome()
+        //{
+        //    return "Este es el metodo de accion de bienvenida.... ";
+        //}
+        public string Welcome2(string name, int numTimes = 1)
+        {
+            return HttpUtility.HtmlEncode("Hello" + name + ", NumTimes is: " + numTimes);                          
+        }
+
+        public string Welcome3(string name, int ID = 1)
+        {
+            return HttpUtility.HtmlEncode("Hello" + name + ", ID: " + ID);
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewBag.Message = "Hello " + name;
+            ViewBag.NumTimes = numTimes;
+
+            return View();
+        }
+
+
+    }
+}
---
+++ MvcMovie/Controllers/MovieModelsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MvcMovie.Models;
+
+namespace MvcMovie.Controllers
+{
+    public class MovieModelsController : Controller
+    {
+        private MovieDbContext db = new MovieDbContext();
+
+        // GET: MovieModels
+        public ActionResult Index()
+        {
+            return View(db.Movies.ToList());
+        }
+
+        // GET: MovieModels/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MovieModel movieModel = db.Movies.Find(id);
+            if (movieModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movieModel);
+        }
+
+        // GET: MovieModels/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: MovieModels/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Title,ReleaseDate,Genre,Price")] MovieModel movieModel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Movies.Add(movieModel);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(movieModel);
+        }
+
+        // GET: MovieModels/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MovieModel movieModel = db.Movies.Find(id);
+            if (movieModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movieModel);
+        }
+
+        // POST: MovieModels/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Title,ReleaseDate,Genre,Price")] MovieModel movieModel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(movieModel).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(movieModel);
+        }
+
+        // GET: MovieModels/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MovieModel movieModel = db.Movies.Find(id);
+            if (movieModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movieModel);
+        }
+
+        // POST: MovieModels/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            MovieModel movieModel = db.Movies.Find(id);
+            db.Movies.Remove(movieModel);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
